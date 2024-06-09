16fe087884773700a2fe6607ba4d05376886426a
Pinapathruni Saikiran <109568252+saikiranpi@users.noreply.github.com>
2024-06-09 11:16:43 +0530

    Create userdata.sh

---
+++ Day32/userdata.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+apt update 
+apt install -y openjdk-8-jdk
+apt install unzip
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
